16ff2c2dffccadaa6ebd6fa9691502e08d1c1c3d
Salavei <kano.mori@mail.ru>
2022-01-24 17:43:32 +0300

    add requirements

--- resize_picture/forms.py
+++ resize_picture/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class ImageForm(forms.Form):
     image_up = forms.ImageField(label='Your image')
-    # width = forms.IntegerField(label='width')
-    # height = forms.IntegerField(label='height')
+    width = forms.IntegerField(label='width')
+    height = forms.IntegerField(label='height',required=False)
--- resize_picture/views.py
+++ resize_picture/views.py
@@ -1,58 +1,30 @@
-# from django.shortcuts import render
-# from django.views.generic import ListView
-#
-# from .models import Image
-# # Create your views here.
-#
-# class ImageView(ListView):
-#     model = Image
-
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from .forms import ImageForm
 from .models import Image
-
-import copy
-
 from PIL import Image as rs_image
 
-import random
+HEIGHT = None
+WIDTH = None
 def image_upload(request):
     context = {}
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
+            WIDTH = form.cleaned_data.get("width")
+            HEIGHT = form.cleaned_data.get("width")
             image_up = form.cleaned_data.get("image_up")
-            # obj = Image.objects.create(image_up = image_up,)
-            # aaaa = str(random.randint(100, 500)) + str(image_up)
-
             obj = Image.objects.create(image_up=image_up, )
-            image_path = f'./userpic/{obj}'
             ss = str(obj)[-3:-1]
             img = rs_image.open(image_up)
-            # # получаем ширину и высоту
-            new_image = img.resize((400, 400))
+            new_image = img.resize((WIDTH, HEIGHT))
             new_image.save(f'./userpic/{ss + str(image_up)}')
-
             obj = Image.objects.update(image_up=ss + str(image_up), )
+            print('\n' * 10, obj, '\n' * 10)
 
-            print('\n' * 10 ,ss ,obj, '\n' * 10)
     else:
         form = ImageForm()
-
-
     context['form']= form
     return render(request, "image.html", context)
 
-# def image_upload(request):
-#     if request.method == 'POST':
-#         form = ImageForm(request.POST)
-#         print(form)
-#         if form.is_valid():
-#             Image(image_up=form).save()
-#             return HttpResponseRedirect('')
-#     else:
-#         form = ImageForm()
-#
-#     return render(request, 'image.html', {'form': form}, print(form))
+
 
