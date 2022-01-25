f5f11dc547f03cd63ace7297461fe72433b4e47b
Salavei <kano.mori@mail.ru>
2022-01-25 12:42:00 +0300

    added a condition to the function image_upload

--- resize_picture/views.py
+++ resize_picture/views.py
@@ -5,10 +5,12 @@ from .models import Image
 
 from PIL import Image as rs_image
 import hashlib
+import time
 
 def resize_img(request):
     link = Image.objects.order_by('-id')[0]
     result = hashlib.md5(str(link).encode())
+    id = Image.objects.filter(image_up=str(link)).values_list('pk', flat=True)
     im = rs_image.open(f'./userpic/{str(link)}')
     (width, height) = im.size
     link_text = f'{result.hexdigest()}_{width}_x_{height}.{str(link)[-3:]}'
@@ -20,16 +22,29 @@ def image_upload(request):
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            WIDTH = form.cleaned_data.get("width")
-            HEIGHT = form.cleaned_data.get("height")
-            image_up = form.cleaned_data.get("image_up")
-            obj = Image.objects.create(image_up=image_up, )
-            ss = str(obj)[-3:-1]
-            img = rs_image.open(image_up)
-            new_image = img.resize((WIDTH, HEIGHT))
-            new_image.save(f'./userpic/{ss + str(image_up)}')
-            obj = Image.objects.update(image_up=ss + str(image_up), )
-            print('\n' * 10, obj, '\n' * 10)
+            if form.cleaned_data.get("height") is None:
+                WIDTH = form.cleaned_data.get("width")
+                HEIGHT = WIDTH
+                image_up = form.cleaned_data.get("image_up")
+                obj = Image.objects.create(image_up=image_up)
+                img = rs_image.open(image_up)
+                new_image = img.resize((WIDTH, HEIGHT))
+                new_name = str(time.time())
+                ff = new_image.save(f'./userpic/{new_name + str(image_up)}')
+                obj = Image.objects.filter(image_up=image_up).update(image_up=(new_name + str(image_up)), )
+                print('\n' * 10, ff, '\n' * 10)
+            else:
+                WIDTH = form.cleaned_data.get("width")
+                HEIGHT = form.cleaned_data.get("height")
+                image_up = form.cleaned_data.get("image_up")
+                obj = Image.objects.create(image_up=image_up, )
+                img = rs_image.open(image_up)
+                new_name = str(time.time())
+                new_image = img.resize((WIDTH, HEIGHT))
+                ff = new_image.save(f'./userpic/{new_name + str(image_up)}')
+                obj = Image.objects.filter(image_up=image_up).update(image_up=(new_name + str(image_up)), )
+                print('\n' * 10, ff, '\n' * 10)
+
 
     else:
         form = ImageForm()
